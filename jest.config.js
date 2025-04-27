f454919eb81754427fd2fbbbe636e88113fb9b7e
Saurabh Gawade <saurabhgawade2920@gmail.com>
2025-04-27 18:24:51 +0530

    Jest basic setup and simple test case

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
+
+import { defaults } from 'jest-config';
+
 export default {
-    testEnvironment: 'jest-environment-jsdom', // Ensure this is set correctly
-    moduleFileExtensions: ['js', 'jsx'],
-    setupFilesAfterEnv: ['@testing-library/jest-dom'],
-    transform: {
-        '^.+\\.(js|jsx)$': 'babel-jest',
+    ...defaults,
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Mock style files
+        '\\.(svg|png|jpg|jpeg|gif|webp|avif|ico|bmp|tiff)$': '<rootDir>/__mocks__/fileMock.js', // Mock image files
     },
+    testEnvironment: 'jsdom', // Ensure Jest uses a DOM-like environment
+    setupFilesAfterEnv: ['@testing-library/jest-dom'], // Extend Jest with custom matchers
 };
+
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,46 +1,13 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-// For Jest testing functions
 import { test, expect } from '@jest/globals';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// Test for checking if "Vite" is rendered
-test('renders the word Vite', () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-  
-  // Check if the word 'Vite' is present in the document
-  expect(screen.getByText(/Vite/i)).toBeInTheDocument();
+test('checks if the word "HMR" is present', () => {
+  render(<App />);
+
+  // Verify the text "Vite" is present
+  const viteText = screen.getByText(/HMR/i);
+  expect(viteText).toBeInTheDocument();
 });
 
-// Test for checking if the initial count value is 0
-test('renders initial count value', () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-  
-  // Check if the count value is initially 0
-  expect(screen.getByText(/count is 0/i)).toBeInTheDocument();
-});
-
-// Test for incrementing the count value when the button is clicked
-test('increments the count value', () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-  
-  const button = screen.getByText(/count is 0/i);
-  
-  // Simulate button click to increment the count
-  fireEvent.click(button);
-  
-  // Check if the count is updated to 1
-  expect(screen.getByText(/count is 1/i)).toBeInTheDocument();
-});
