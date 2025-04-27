309cde090e3e30e49fd2e73ec649cbdb836bc788
Saurabh Gawade <saurabhgawade2920@gmail.com>
2025-04-27 23:57:41 +0530

    React Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+//! Router-Provider
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+//! Layout
+import Rootlayout from './components/layout/Rootlayout/Rootlayout';
+//! Pages
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import NotFound from './pages/NotFound/NotFound';
+import "./App.css"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Rootlayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "contact", element: <Contact /> },
+    ],
+  },
+  { path: "*", element: <NotFound /> }, // Catch-all for 404s
+]);
 
 const App = () => {
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
