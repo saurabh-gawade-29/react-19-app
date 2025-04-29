7a742cede7174cc805844d7edc9fde31ccaa1703
Saurabh Gawade <saurabhgawade2920@gmail.com>
2025-04-29 23:34:15 +0530

    api added

---
+++ src/components/common/CButton/CButton.jsx
@@ -0,0 +1,29 @@
+import React, { forwardRef } from "react";
+import "./GButton.scss";
+import PropTypes from "prop-types";
+import { Link } from "lucide-react";
+
+const CButton = forwardRef((props, ref) => {
+    return (
+        <button
+            disabled={props.disabled}
+            className={props.className}
+            role={props.role}
+            ref={ref}
+            onClick={props.onClick}
+        >
+            {props.label}
+        </button>
+    );
+});
+
+CButton.propTypes = {
+    label: PropTypes.string,
+    onClick: PropTypes.func,
+    disabled: PropTypes.bool,
+};
+
+CButton.defaultProps = {
+    label: "Submit",
+};
+export default CButton;
