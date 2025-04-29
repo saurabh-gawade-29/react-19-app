92c24e9822693e5923961d18644cf59f515b376b
Saurabh Gawade <saurabhgawade2920@gmail.com>
2025-04-29 22:55:03 +0530

    update Hero Section

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -5,14 +5,14 @@ const Navbar = () => {
     return (
         <>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div class="container-fluid">
-                    <NavLink to="/" className="nav-link text-light fw-bold">
+                <div class="container">
+                    <NavLink to="/" className="nav-link text-light fw-bold ps-0 pe-0">
                         COUNTRY ATLAS
                     </NavLink>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
                                 <NavLink to="/" className="nav-link">Home</NavLink>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import tourImage from '../../assets/images/travel-concept-with-worldwide-landmarks-removebg-preview.png'
+import "./Home.css"
 const Home = () => {
     return (
         <>
-            <div>Home</div>
+            {/* Hero Section */}
+            <div className="">
+                <div className="row">
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <div className="d-flex align-items-center wrapHeroContent">
+                            <div className="heroContent">
+                                <h1 className="display-3 caGap">
+                                    Explore the World, One Country at a Time
+                                </h1>
+                                <h6 className="mb-0 caGap">
+                                    Discover the history, culture and beauty of every nation. Sort, search and filter through countries to find the details you need.
+                                </h6>
+                                <button className="caButton caGap px-4">Start Exploring</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <div className="d-flex align-items-center wrapHeroContent">
+                            <img className="img-fluid tourImg" src={tourImage} alt="tourImage" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
